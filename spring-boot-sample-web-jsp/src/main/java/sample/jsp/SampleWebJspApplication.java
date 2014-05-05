65acbdecb0fefad648088b39ad22a1adce459942
piotr.heftowicz <piotr.heftowicz@zencard.pl>
2014-05-05 22:42:56 +0200

    -Aplikacja nie startuje samoczynnie bazy H2.

--- spring-boot-sample-web-jsp/src/main/java/sample/jsp/SampleWebJspApplication.java
+++ spring-boot-sample-web-jsp/src/main/java/sample/jsp/SampleWebJspApplication.java
@@ -16,22 +16,15 @@
 
 package sample.jsp;
 
-import java.sql.SQLException;
 import java.util.Arrays;
 
-import javax.sql.DataSource;
-
-import org.h2.Driver;
-import org.h2.tools.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.DependsOn;
 
 @Configuration
 @EnableAutoConfiguration
@@ -58,31 +51,5 @@ public class SampleWebJspApplication extends SpringBootServletInitializer {
             System.out.println(beanName);
         }
     }
-
-    @Bean(name = "dataSource")
-    @DependsOn("h2Server")
-    public DataSource dataSource() {
-        org.apache.tomcat.jdbc.pool.DataSource dataSource = new org.apache.tomcat.jdbc.pool.DataSource();
-        dataSource.setDriverClassName(Driver.class.getName());
-        dataSource.setUsername("sa");
-        dataSource.setUrl("jdbc:h2:tcp://localhost/~/test");
-        return dataSource;
-    }
-
-    @Bean(name="h2Server")
-    public Server h2Server() throws SQLException {
-        Server server = Server.createTcpServer("-tcpPort", "9092", "-tcpAllowOthers").start();
-        return server;
-    }
-
-    @Bean(name="h2WebServer")
-    @DependsOn("h2Server")
-    public Server h2WebServer() throws SQLException {
-        Server webServer = Server.createWebServer("-webAllowOthers","-webPort","8082").start();
-        return webServer;
-    }
-
-
-
 
 }
